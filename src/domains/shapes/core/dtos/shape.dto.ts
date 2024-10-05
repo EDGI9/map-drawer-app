7639698c0ae4f828c4a1435d3037d38e26c6c90f
EDGI9 <dias.s.edgar@gmail.com>
2024-10-05 17:07:03 +0000

    Create of base Mocks for shapes and maps along with the typescript interfaces

---
+++ src/domains/shapes/core/dtos/shape.dto.ts
@@ -0,0 +1,61 @@
+type fillColorStops = { 
+    offset: number, 
+    color: string, 
+    opacity: number
+} 
+
+type shapeShadow = {
+    affectStroke:boolean
+    blur:number
+    color:string
+    nonScaling:boolean
+    offsetX:number
+    offsetY:number
+}
+
+type shapeFill = string | {
+    colorStops: fillColorStops[],
+    coords: {x1: number, y1: number, x2: number, y2: number},
+    gradientTransform: number[],
+    gradientUnits:string,
+    offsetX:number,
+    offsetY:number,
+    type:string
+}
+
+export interface shapeDTO {
+    angle: number,
+    backgroundColor: string,
+    fill: string | shapeFill,
+    fillRule: string,
+    flipX:boolean,
+    flipY:boolean,
+    globalCompositeOperation:string,
+    height:number,
+    left:number,
+    opacity:number,
+    originX:string,
+    originY:string,
+    paintFirst:string,
+    rx?:number,
+    ry?:number,
+    path?:(string|number)[][],
+    scaleX:number,
+    scaleY:number,
+    shadow: shapeShadow | null,
+    skewX:number,
+    skewY:number,
+    stroke:string | null,
+    strokeDashArray: string[] | null,
+    strokeDashOffset:number,
+    strokeLineCap:string,
+    strokeLineJoin:string,
+    strokeMiterLimit:number,
+    strokeUniform:boolean,
+    strokeWidth:number,
+    top:number,
+    type:string,
+    version:string,
+    visible:boolean,
+    width:number,
+}
