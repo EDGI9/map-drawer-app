bd69a5db3e41b279d064c560006f5eeb0efa6267
EDGI9 <dias.s.edgar@gmail.com>
2024-10-05 18:19:54 +0000

    Creating unit test for Map service and fixing typo in mock data

--- src/domains/maps/__mocks__/maps.ts
+++ src/domains/maps/__mocks__/maps.ts
@@ -1,6 +1,6 @@
-import { mapDTO } from "../core/dtos/map.dto";
+import { MapDTO } from "../core/dtos/map.dto";
 
-export const imageList: mapDTO[] = [
+export const MapList: MapDTO[] = [
     {
         id: "aaaa-aaaa",
         src: "https://sentinel-cogs.s3.us-west-2.amazonaws.com/sentinel-s2-l2a-cogs/36/Q/WD/2020/7/S2A_36QWD_20200701_0_L2A/TCI.tif"
---
+++ src/domains/maps/__tests__/maps.spec.ts
@@ -0,0 +1,6 @@
+import {describe, it} from 'vitest';
+describe('Maps Service', () => {
+    it('Get map by id', async () => {});
+
+    it('Returns null if no id', async () => {});
+})
