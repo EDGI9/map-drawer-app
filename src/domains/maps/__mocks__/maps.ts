2c7156547dcaf1834903651ac1562f327c611ff0
EDGI9 <dias.s.edgar@gmail.com>
2024-10-06 18:22:12 +0000

    Add face functionality to fetch the data and display it on page, including map, shapes and some functionality (WIP)

--- src/domains/maps/__mocks__/maps.ts
+++ src/domains/maps/__mocks__/maps.ts
@@ -1,8 +1,9 @@
 import { MapDTO } from "../core/dtos/map.dto";
+import image from "../../../assets/TCI_1.jpg";
 
 export const MapList: MapDTO[] = [
     {
         id: "aaaa-aaaa",
-        src: "https://sentinel-cogs.s3.us-west-2.amazonaws.com/sentinel-s2-l2a-cogs/36/Q/WD/2020/7/S2A_36QWD_20200701_0_L2A/TCI.tif"
+        src: image
     }
 ] 
--- src/views/MapPage.tsx
+++ src/views/MapPage.tsx
@@ -1,36 +1,111 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from 'react'
+import { fabric } from "fabric";
+import { FabricJSCanvas, useFabricJSEditor } from "fabricjs-react";
 import MapService from "../domains/maps/index";
 import { MapList } from "../domains/maps/__mocks__/maps";
 import ShapesService from "../domains/shapes";
+import { ShapeDTO } from '../domains/shapes/core/dtos/shape.dto';
+import { MapDTO } from '../domains/maps/core/dtos/map.dto';
 
 
 function MapPage() {
-    
-    async function getMap() {
-        const map = await MapService.getById(MapList[0].id);
+    let [map, setMap] = useState<MapDTO>();
+    let [shapesList, setShapesList] = useState<ShapeDTO[]>();
+    const { editor, onReady } = useFabricJSEditor();
 
-        if (!map) {
+
+    const onAddRectangle = () => {
+        if (!editor || !fabric) {
+            return;
+        }
+        
+        editor.addRectangle();
+    };
+
+    function setCanvasProperties() {
+        if (!editor || !fabric) {
+            return;
+        }
+        editor.canvas.setHeight(1000);
+        editor.canvas.setWidth(1000);
+        editor.canvas.renderAll();
+    }
+
+    async function loadMap() {
+        const resultMap = await MapService.getById(MapList[0].id);
+        
+        if (!resultMap) {
             console.log('Map not found');
             return  
         }
-        
-        const shapes = await ShapesService.getById(map.id)
-        console.log(map, shapes);
+        setMap(resultMap)
     }
 
-    useEffect(() => {
-        getMap()
-    }, [])
+    async function loadShapes() {
+        if (!map) {
+            console.log('No Map data');
+            return  
+        }
+        const resultsShapes = await ShapesService.getById(map.id)
 
+        if (!resultsShapes) {
+            console.log('Shapes not found');
+            return  
+        }
+
+        setShapesList(resultsShapes)
+    }
+
+
+    useEffect(() => {
+        loadShapes(); 
+    }, [map]);
+
+    useEffect(() => {
+        if (!shapesList) {
+            return   
+        }
+
+        if (!editor || !fabric) {
+            return;
+        }
+        shapesList.forEach((shape) => {
+            if (shape.type == "rect") {
+                const rect = new fabric.Rect(shape);
+            
+                editor.canvas.add(rect)
+            }
+
+            if (shape.type == "path") {
+                const path = new fabric.Path(shape.path);
+                path.set(shape)
+            
+                 editor.canvas.add(path)
+            }
+        })
+    }, [shapesList]);
+
+    useEffect(() => {
+        setCanvasProperties();
+        loadMap();
+    }, []);
+
+    
     return(
         <div>
             <h1>Map area</h1>
             <div>
-                <button>Add Rect</button>
+                <button onClick={() => onAddRectangle()}>Add Rect</button>
                 <button>Add Polygon</button>
             </div>
-            <div>
-                {/* map location */}
+            <div className='c-drawing-container'>
+                <FabricJSCanvas className="c-canvas" onReady={onReady} />
+                {map ? (
+                    <img src={map.src} height={400} width={400} className='c-image'/>
+                    ) : (
+                    <p>Loading...</p>
+                    )
+                }
             </div>
         </div>
     )
