56e3adb56b51340b161f3100557a0e9dcb1f03c4
EDGI9 <dias.s.edgar@gmail.com>
2024-10-06 08:58:09 +0000

    Create base unit test for Shapes service

---
+++ src/domains/shapes/__tests__/shapes.spec.ts
@@ -0,0 +1,19 @@
+import { describe, it } from 'vitest';
+
+describe('Shapes service', () => {
+    it('Can get shapes data', () => {
+
+    })
+    it('Can create shape', () => {
+
+    });
+    it('Can update shape', () => {
+
+    });
+    it('Can delete shape', () => {
+
+    });
+    it('Can send shapes data', () => {
+
+    });
+})
