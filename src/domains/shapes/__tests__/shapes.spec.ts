8fac7b2829eefb3f335f22810460c4b5ce5e0505
EDGI9 <dias.s.edgar@gmail.com>
2024-10-06 09:51:15 +0000

    Adding unit test for Shapes Service

--- src/domains/shapes/__tests__/shapes.spec.ts
+++ src/domains/shapes/__tests__/shapes.spec.ts
@@ -1,19 +1,80 @@
-import { describe, it } from 'vitest';
+import { describe, it, expect, vi } from 'vitest';
+
+import { MapList }  from "../../maps/__mocks__/maps";
+import { ShapeList }  from "../__mocks__/shapes";
+import { ShapeDTO } from '../core/dtos/shape.dto';
 
 describe('Shapes service', () => {
-    it('Can get shapes data', () => {
+    it('Can get shapes data', async () => {
+        expect(ShapesService.getById).toBeDefined();
+        expect(ShapesService.getById).toBeInstanceOf(Function);
 
-    })
-    it('Can create shape', () => {
+        const mapId = MapList[0].id;
+        const spy = vi.spyOn(ShapesService, "getById");
+
+        const result = await ShapesService.getById(mapId);
+
+        expect(spy).toHaveBeenCalledOnce();
+
+        expect(result).toStrictEqual(
+            expect.arrayContaining(<ShapeDTO[]>[
+                expect.objectContaining(<ShapeDTO>{
+                    angle: expect.any(Number),
+                    backgroundColor: expect.any(String),
+                    fill: expect.any(String),
+                    fillRule: expect.any(String),
+                    flipX:expect.any(Boolean),
+                    flipY:expect.any(Boolean),
+                    globalCompositeOperation:expect.any(String),
+                    height:expect.any(Number),
+                    left:expect.any(Number),
+                    opacity:expect.any(Number),
+                    originX:expect.any(String),
+                    originY:expect.any(String),
+                    paintFirst:expect.any(String),
+                    rx: expect.any(Number) || expect.anything(), 
+                    ry: expect.any(Number) || expect.anything(),
+                    path:(expect.any(String) || expect.arrayContaining(expect.arrayContaining([expect.any(String), expect.any(Number)]))) || expect.anything(),
+                    scaleX:expect.any(Number),
+                    scaleY:expect.any(Number),
+                    shadow: expect.objectContaining({
+                        affectStroke:expect.any(Boolean),
+                        blur:expect.any(Number),
+                        color:expect.any(String),
+                        nonScaling:expect.any(Boolean),
+                        offsetX:expect.any(Number),
+                        offsetY:expect.any(Number),
+                    }) || expect(null),
+                    skewX:expect.any(Number),
+                    skewY:expect.any(Number),
+                    stroke:expect.any(expect.any(String),) || expect(null),
+                    strokeDashArray: expect.arrayContaining([expect.any(String)]) || expect(null),
+                    strokeDashOffset:expect.any(Number),
+                    strokeLineCap:expect.any(String),
+                    strokeLineJoin:expect.any(String),
+                    strokeMiterLimit:expect.any(Number),
+                    strokeUniform:expect.any(Boolean),
+                    strokeWidth:expect.any(Number),
+                    top:expect.any(Number),
+                    type:expect.any(String),
+                    version:expect.any(String),
+                    visible:expect.any(Boolean),
+                    width:expect.any(Number),
+                })
+            ])
+        )
 
     });
-    it('Can update shape', () => {
+    it('Can send shapes data', async () => {
+        expect(ShapesService.update).toBeDefined();
+        expect(ShapesService.update).toBeInstanceOf(Function);
 
-    });
-    it('Can delete shape', () => {
+        const mapId = MapList[0].id;
+        const spy = vi.spyOn(ShapesService, "update");
 
-    });
-    it('Can send shapes data', () => {
-
+        await ShapesService.update(mapId, ShapeList);
+        
+        expect(spy).toHaveBeenCalledOnce();
+        expect(spy).toHaveBeenCalledWith(mapId, ShapeList);
     });
 })
